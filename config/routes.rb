fcfae05fce6713c35163d2e826c46fb8a99f3cbc
Linda Keating <linda@secondage.com>
2011-08-15 22:17:51 +0100

    Added links with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Crowdshare::Application.routes.draw do
+
+
+  root :to => 'pages#home'
+  match "/contact", :to => "pages#contact"
+  match "/about", :to => "pages#about"
+  match "/help", :to => "pages#help"
+
   get "pages/home"
-  get "pages/about"
-  get "pages/contact"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +58,7 @@ Crowdshare::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -37,15 +37,27 @@ describe PagesController do
     end
   end
 
-   describe "GET 'about'" do
+  describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-     it 'should have the right title' do
+    it 'should have the right title' do
       get 'about'
       response.should have_selector("title",
                       :content => "#{@base_title} | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it 'should have the right title' do
+      get 'help'
+      response.should have_selector("title",
+                      :content => "#{@base_title} | Help")
     end
   end
 
