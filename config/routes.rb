37f1a85ac6e3715354ebaa8f215b5b8ea6ee0e4b
Linda Keating <linda@secondage.com>
2011-08-15 17:43:46 +0100

    added a pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Crowdshare::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
