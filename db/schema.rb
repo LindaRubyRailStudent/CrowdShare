5c7946ff6f87a65cd9507ea7f0dd16dde48b7248
Linda Keating <linda@secondage.com>
2011-08-16 14:58:04 +0100

    finished the password security

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110816124354) do
+ActiveRecord::Schema.define(:version => 20110816133023) do
 
   create_table "cs_members", :force => true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20110816124354) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
