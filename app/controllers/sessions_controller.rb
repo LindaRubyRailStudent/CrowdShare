62ca14b59d687d727fffe4198a190b0a3904a13f
Linda Keating <linda@secondage.com>
2011-08-18 20:50:49 +0100

    working session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,8 +14,8 @@ class SessionsController < ApplicationController
       flash.now[:error] ="Invalid email/password combination"
       render 'new'
     else
-      # Handle successful sign in here
+      sign_in user
+      redirect_to user
     end
-
   end
 end
