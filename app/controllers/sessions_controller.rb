3f06185046141daff567d8f59e4350696dffeeed
Linda Keating <linda@secondage.com>
2011-08-18 23:58:20 +0100

    Sessions with some failing tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    @title = "Sign out"
+    sign_out
+    redirect_to root_path
   end
 
   def create
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -101,6 +101,11 @@ describe UsersController do
         post :create, :user => @attr
         flash[:success].should =~ /Welcome to Crowdshare App/
       end
+
+      it "should sign user in" do
+        post :create, :user =>@attr
+        controller.should be_signed_in
+      end
     end
   end
 end
